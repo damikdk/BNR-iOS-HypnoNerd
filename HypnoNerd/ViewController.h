0fd20a595bc9d2d17637ffc93e4513a7c5352373
damikdk <damikdk@gmail.com>
2017-02-08 18:10:04 +0300

    Initial Commit

---
+++ HypnoNerd/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HypnoNerd
+//
+//  Created by admin on 08.02.17.
+//  Copyright © 2017 in-tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
